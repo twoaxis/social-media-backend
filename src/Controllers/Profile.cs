0c4c7b0059d7e714ed9907fe7c3b7822d57e9d68
Rynvasis <ahmedraeda66@gmail.com>
2024-10-21 22:17:14 +0300

    Add ProfileController
    This commit add ProfileController and add function to check if username
    is found or not

---
+++ src/Controllers/Profile.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using social_media_backend.Services;
+
+namespace social_media_backend.src.Controllers
+{
+    [Route("/profile")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private string GetUserProfile(string username)
+        {
+            DatabaseService.OpenConnection();
+
+            try
+            {
+                using (var command = new MySqlCommand("SELECT username FROM users WHERE username = @username", DatabaseService.Connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader.GetString("username");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                DatabaseService.CloseConnection();
+            }
+            return null;
+        }
+    }
+}
