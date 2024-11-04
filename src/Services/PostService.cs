5405adfe7b9309acbab4250befb097d941693d4a
Mohamed Eldahtoury <salintofficial@hotmail.com>
2024-11-04 12:44:12 +0200

    Profile enhancement (#29)
    * feat(profile): return user posts in profile
    
    This commit creates a few models regarding user profile, post, and post
    author and then returns all user posts to the profile endpoint.
    
    Refs: #22
    
    * feat(profile): add follower and following count
    
    This commit uses subqueries to get the follower and following count in
    the user profile to be displayed.

--- src/Services/PostService.cs
+++ src/Services/PostService.cs
@@ -1,4 +1,5 @@
 using MySql.Data.MySqlClient;
+using social_media_backend.Models.Post;
 
 namespace social_media_backend.Services;
 
@@ -23,5 +24,38 @@ public class PostService
 		{
 			DatabaseService.CloseConnection();
 		}
+	}
+
+	public Post[] GetAllPosts(int userId)
+	{
+		List<Post> posts = [];
+		DatabaseService.OpenConnection();
+
+		try
+		{
+			using var command = new MySqlCommand("SELECT posts.id, posts.content, posts.createdAt, posts.createdAt, users.id AS uid, users.username, users.name FROM posts JOIN users ON posts.author = users.id WHERE users.id = @id ORDER BY posts.createdAt DESC;", DatabaseService.Connection);
+			command.Parameters.AddWithValue("@id", userId);
+			
+			using var reader = command.ExecuteReader();
+			while (reader.Read())
+			{
+				posts.Add(new Post(
+					reader.GetInt32("id"),
+					reader.GetString("content"),
+					reader.GetDateTime("createdAt"),
+					new PostAuthor(
+						reader.GetInt32("uid"),
+						reader.GetString("username"),
+						reader.GetString("name")
+					)
+				));
+			}
+		}
+		finally
+		{
+			DatabaseService.CloseConnection();
+		}
+		
+		return posts.ToArray();
 	}
 }
--- src/Services/UserService.cs
+++ src/Services/UserService.cs
@@ -1,4 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
+using social_media_backend.Models.Post;
+using social_media_backend.Models.User;
 using social_media_backend.Services;
 using social_media_backend.src.Exceptions;
 
@@ -6,34 +8,46 @@ namespace social_media_backend.src.Services
 {
     public class UserService
     {
-        /*
-         * TODO: Should have a model created for it.
-         * - Create a model that represents a full user called `UserProfile`.
-         */
-        public (string UserName, string Name) GetUserProfile(string username)
+        private readonly PostService _postService = new();
+        
+        public UserProfile GetUserProfile(string username)
         {
+            UserProfile profile;
             DatabaseService.OpenConnection();
 
-            if (!DoesUserExistByUsername(username))
+            try
             {
-                DatabaseService.CloseConnection();
-                throw new UserNotFoundException();
-            }
+                if (!DoesUserExistByUsername(username))
+                {
+                    throw new UserNotFoundException();
+                }
 
-            using (var command = new MySqlCommand("SELECT username, name FROM users WHERE username = @username", DatabaseService.Connection))
-            {
+                using var command = new MySqlCommand("SELECT u.id, u.username, u.name, (SELECT COUNT(*) FROM follows WHERE follower_id = u.id) AS following_count, (SELECT COUNT(*) FROM follows WHERE following_id = u.id) AS follower_count FROM users u WHERE u.username = @username;",
+                    DatabaseService.Connection);
                 command.Parameters.AddWithValue("@username", username);
 
-                using (var reader = command.ExecuteReader())
+                using var reader = command.ExecuteReader();
+                if (reader.Read())
                 {
-                    if (reader.Read())
-                    {
-                        return (reader.GetString("username"), reader.GetString("name"));
-                    }
+                    profile = new UserProfile(
+                        reader.GetInt32("id"),
+                        reader.GetString("username"),
+                        reader.GetString("name"),
+                        reader.GetInt32("follower_count"),
+                        reader.GetInt32("following_count"),
+                        []
+                    );
+                    reader.Close();
                 }
+                else throw new UserNotFoundException();
+
+                profile.posts = _postService.GetAllPosts(profile.id);
             }
-            DatabaseService.CloseConnection();
-            return (null, null);
+            finally
+            {
+                DatabaseService.CloseConnection();
+            }
+            return profile;
         }
         public bool DoesUserExistByUsername(string username)
         {
